170dc05a713d5fcec773affec8c27c0200d15835
Veronika <veroniksayas@gmail.com>
2022-08-06 18:54:37 +0300

    Created responsive menu

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const open = document.querySelector('.burger')
+const close = document.querySelector('.close-menu')
+const inner = document.querySelector('.header__wrapper-inner')
+const overlay = document.querySelector('.overlay')
+const body = document.querySelector('body')
+
+open.addEventListener('click', () => {
+    inner.classList.add('active')
+    overlay.classList.add('active')
+    body.classList.add('overflow-hidden')
+})
+
+close.addEventListener('click', () => {
+    inner.classList.remove('active')
+    overlay.classList.remove('active')
+    body.classList.remove('overflow-hidden')
+})
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 767){
+        overlay.classList.remove('active')
+        inner.classList.remove('active')
+        body.classList.remove('overflow-hidden')
+    }
+})
